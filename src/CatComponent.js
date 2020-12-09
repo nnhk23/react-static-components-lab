6cf98adbf1d06399ef37711aa11d2fee35c80028
Kim Nguyen <nnhk23@gmail.com>
2020-12-09 12:28:35 -0800

    Done.

--- src/CatComponent.js
+++ src/CatComponent.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent'
 
 export default class CatComponent extends Component {
   render() {
     return (
       <div className="bar" id="cat">
         <img src="/cat.gif" />
+        <GraceHopperQuoteComponent />
       </div>
     );
   }
